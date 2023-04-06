49faa6d269c901224f70d790480864a78ba918eb
retsu-wakasa <retsu.wakasa@gmail.com>
2023-04-07 08:35:03 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ $("#addTodo").click(function(){
 
     $("input").val(""); //挿入後に入力ボックスを空にする
 });
+
+$(document).on("change", "input[type=checkbox",function(){
+    if ($(this).is(":checked")){
+        $(this).parent().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+    }
+});
+
